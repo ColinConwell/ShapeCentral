3ae4c748d9d40833947badd89fd5183550760440
ColinConwell <conwell@g.harvard.edu>
2019-07-09 00:26:19 +0100

    Update shape_render.py

--- shape_render.py
+++ shape_render.py
@@ -3,5 +3,7 @@ sys.path.append(os.path.dirname(bpy.data.filepath))
 from shape_central import *
 
 output_dir = bpy.path.abspath('//') + 'Render-Repo/SimpleSet'
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
 save_code_archive(output_dir)
 group_render(output_dir, number_of_renders=1000)
